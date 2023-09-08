2108ed131083c6e631d4e4fa81830624e2373094
Igor <iluzarraga@gmail.com>
2023-09-08 14:57:11 +0200

    Fix problem with footer in movil devices

--- src/scenes/footer/Footer.tsx
+++ src/scenes/footer/Footer.tsx
@@ -3,11 +3,11 @@ import SocialMediaIcons from "../../components/SocialMediaIcons"
 
 const Footer = () => {
     return (
-        <footer className="h-32 bg-footer-color pt-10">
+        <footer className="h-36 bg-footer-color pt-10">
             <div className="mx-auto w-5/6">
 
-                <div className="md:flex md:justify-between text-center">
-                    <p className="uppercase font-playfair text-2xl text-white">
+                <div className="flex flex-col gap-4 md:flex-row md:justify-between items-center text-center">
+                    <p className="hidden md:block uppercase font-playfair text-2xl text-white">
                         Igor <span className="text-purple-500">Luzarraga</span>
                     </p>
                     <SocialMediaIcons />
